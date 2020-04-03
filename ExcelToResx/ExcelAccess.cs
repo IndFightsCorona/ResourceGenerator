9b80e158478479adb45723e14957376a3bc328d7
Sriram <sriram5052@hotmail.com>
2020-04-03 17:47:02 +0530

    initial commit with full code

---
+++ ExcelToResx/ExcelAccess.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelToResx
+{
+    class ExcelAccess
+    {
+        private string SourceFile { get; }
+        private OleDbConnection OledbConn { get; set; }
+        public OleDbCommand OledbCmd { get; private set; }
+
+        public ExcelAccess()
+        {
+            SourceFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "SourceFile", @"FightCorona_ResourceValues.xlsx");
+            InitializeOledbConnection();
+        }
+        private void InitializeOledbConnection()
+        {
+            string connString = String.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=""Excel 12.0 Xml;HDR=YES""", SourceFile);
+            OledbConn = new OleDbConnection(connString);
+        }
+        public DataSet GetExcelWorkSheetsName()
+        {
+            OledbConn.Open();
+            DataTable mySheets = OledbConn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+            OledbConn.Close();
+            DataSet ds = new DataSet();
+            DataTable dt;
+
+            for (int i = 0; i < mySheets.Rows.Count; i++)
+            {
+                dt = GetExcelDataAsTable(mySheets.Rows[i]["TABLE_NAME"].ToString());
+                ds.Tables.Add(dt);
+            }
+            return ds;
+        }
+
+        public DataTable GetExcelDataAsTable(string WorkSheetName)
+        {
+            try
+            {
+
+                DataTable dt = new DataTable();
+                using (OleDbCommand comm = new OleDbCommand())
+                {
+                    comm.CommandText = $"Select * from [{WorkSheetName}]";
+                    comm.Connection = OledbConn;
+                    using (OleDbDataAdapter da = new OleDbDataAdapter())
+                    {
+                        da.SelectCommand = comm;
+                        da.Fill(dt);
+                        dt.TableName = WorkSheetName.Remove(WorkSheetName.Length-1);
+                        return dt;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+        }
+    }
+}
---
+++ ExcelToResx/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Resources;
+using System.Text.RegularExpressions;
+
+namespace ExcelToResx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            ExcelAccess exclInstance = new ExcelAccess();
+            DataSet dataSet = exclInstance.GetExcelWorkSheetsName();
+            string oututFolder = ConfigurationManager.AppSettings["OutputFolder"];
+            if (Directory.Exists(oututFolder))
+                Directory.Delete(oututFolder, true);
+            GenerateResxFile(dataSet, oututFolder);
+        }
+        private static void GenerateResxFile(DataSet dataSet, string oututFolder)
+        {
+            foreach (DataTable dtEcelData in dataSet.Tables)
+            {
+                if (!Directory.Exists(Path.Combine(oututFolder, String.Format($"{dtEcelData.TableName}"))))
+                    Directory.CreateDirectory(Path.Combine(oututFolder, String.Format($"{dtEcelData.TableName}")));
+                foreach (DataColumn excelColumn in dtEcelData.Columns)
+                {
+
+                    if (excelColumn.ColumnName != "Key" && excelColumn.ColumnName != "Default Value" && !Regex.IsMatch(excelColumn.ColumnName, "^[A-Za-z]{1}[0-9]{1}"))
+                    {
+                        string fileName = Path.Combine(oututFolder, String.Format($"{dtEcelData.TableName}/{dtEcelData.TableName}.{excelColumn.ColumnName.ToLower()}.resx"));
+
+                        using (ResXResourceWriter resx = new ResXResourceWriter(fileName))
+                        {
+                            foreach (DataRow dRow in dtEcelData.Rows)
+                            {
+                                if (!string.IsNullOrEmpty(dRow.Field<string>("Key")) && !string.IsNullOrEmpty(dRow.Field<string>(excelColumn.ColumnName)))
+                                    resx.AddResource(dRow.Field<string>("Key"), dRow.Field<string>(excelColumn.ColumnName));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
